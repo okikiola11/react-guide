a3fedc963536287272e191c7e4617bcf728e7e39
User <user@Users-MacBook-Pro.local>
2022-02-19 13:05:59 +0100

    Add chart to the components folder Add the chartbar to the chart component to create the bars in the chart component file Call the chart from the ExpensesChart component file'

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import ExpensesFilter from './ExpensesFilter';
 import ExpensesList from './ExpensesList';
 
 import './Expenses.css';
+import ExpensesChart from './ExpensesChart';
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020');
@@ -19,6 +20,7 @@ const Expenses = (props) => {
   return (
     <Card className='expenses'>
       <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList items={filteredExpenses} />
     </Card> 
   )
